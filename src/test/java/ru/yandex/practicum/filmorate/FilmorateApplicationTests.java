82484f27bc8ce543023a73a64b74dda7c87eada0
Kate Mahova <kate.mahova@yandex.ru>
2022-06-30 09:10:00 +0300

    feat: feature/add-common-films after postman test

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -113,14 +113,19 @@ class FilmorateApplicationTests {
 
 	@Test
 	public void testGetCommonFilms() {
-		User user2com = new User("user2@ya.ru", "loginUser2",
-				"nameUser2", LocalDate.of(2000, 01,01));
+		User user2Com = new User("user2@ya.ru", "loginUser2",
+				"nameUser2", LocalDate.of(2000, 1,1));
+		Film film2Com = new Film(0,"namefilm2Com", "description film2Com",
+				120L,LocalDate.of(2010, 3, 3),
+				new MPA(2, "R"), Set.of(new Genre(4, "Триллер")), 8);
 		userStorage.create(user);
-		userStorage.create(user2com);
+		userStorage.create(user2Com);
 		filmStorage.create(film);
+		filmStorage.create(film2Com);
 		like.addLike(film.getId(), user.getId());
-		like.addLike(film.getId(), user2com.getId());
-		Collection<Film> inspection = filmStorage.getCommonFilms(user.getId(), user2com.getId());
+		like.addLike(film.getId(), user2Com.getId());
+		like.addLike(film2Com.getId(), user.getId());
+		Collection<Film> inspection = filmStorage.getCommonFilms(user.getId(), user2Com.getId());
 		Assertions.assertEquals(1, inspection.size());
 	}
 }
